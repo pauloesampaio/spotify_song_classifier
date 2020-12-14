5c0e336b1303b6ff416679d4e099ded8c06b1846
pauloesampaio <paulo.ed@gmail.com>
2020-12-14 17:17:37 -0300

    refactoring

--- credentials.py
+++
@@ -1,8 +0,0 @@
-credentials_dict = {
-    "CLIENT_ID": "XXXXXXXXXXXXXXX",
-    "CLIENT_SECRET": "XXXXXXXXXXXXXXXX",
-    "CLIENT_URI": "XXXXXXXXXXXXXXXX",
-    "USER_1": "XXXXXXXXXXXX",
-    "USER_2": "XXXXXXXXXXXXXX",
-    "SCOPE": 'user-library-read',
-}
